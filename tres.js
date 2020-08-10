3894ff012c35c46c13ef0b89cfce6d6d174be4ed
Ana Carolina Barragán <anabarragan3312@gmail.com>
2020-08-10 20:47:07 -0300

    Nuevo cambio

--- tres.js
+++ tres.js
@@ -11,5 +11,61 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	
+let sexo;
+let sexmax=0;
+let lugar;
+let temp;
+let cantpersonas;
+let cantmax=0;
+let contbari=0;
+let contcata=0;
+let contsalta=0;
+let flag=0;
+let prominv;
+let cantinvierno=0;
+let respuesta="s";
+while(respuesta=="s")
+{
+	do{
+    sexo=prompt("Ingrese el sexo del titular");
+	}while(sexo!="m"&&sexo!="f");
+	do{
+     lugar=prompt("Ingrese un destino");
+	}while(lugar!="bariloche"&&lugar!="cataratas"&&lugar!="salta");
+    do{
+     temp=prompt("Ingrese la temporada ");
+	}while(temp!="otoño"&&temp!="verano"&&temp!="invierno");
+	do{
+    cantpersonas=parseInt(prompt("Ingrese la cantidad de personas que viajan"));
+	}while(cantpersonas<0&&isNaN(cantpersonas));
+    
+ respuesta=prompt("Desea registrar otro viaje? s/n");
+ switch(lugar)
+ {
+	 case"bariloche":
+	 contbari++;
+	 break;
+	 case"salta":
+	 contsalta++;
+	 break;
+	 case"cataratas":
+	 contcata++;
+	 break;
+
+ }
+ if(flag==0||cantmax<cantidad)
+ {
+	 cantmax=cantidad;
+	 sexmax=sexo;
+	 flag=1;
+
+ }
+ if()
+ {
+
+ }
+}	
+document.write("");
+document.write("El sexo del titular que lleva más pasajeros: " +sexmax+"<br>");
+document.write("");
 }
