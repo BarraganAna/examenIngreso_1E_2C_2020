2a0edf7bd75de67e7288f253ed51a835e602bce3
Ana Carolina Barragán <anabarragan3312@gmail.com>
2020-08-10 20:09:29 -0300

    Nuevo cambio

--- dos.js
+++ dos.js
@@ -5,7 +5,48 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
-}
+*/function mostrar() {
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let pesototal = 0;
+  let marcacaro;
+  let premax = 0;
+  let premin = 0;
+  let marcabarato;
+  let flag = 0;
+  let flagS = 0;
+  let respuesta = "s";
+  while (respuesta == "s") {
+    marca = prompt("Ingrse la marca");
+    do {
+      precio = parseInt(prompt("Ingrese el precio"));
+
+    } while (isNaN(precio) || precio < 0);
+    peso = parseFloat(prompt("Ingrese el peso en kg"));
+    do {
+      tipo = prompt("Ingrese el tipo");
+    } while (tipo != "solido" && tipo != "liquido");
+    respuesta = prompt("Desea ingresar otro producto? s/n");
+
+    pesototal = pesototal + peso;
+
+    if ((flag == 0 && tipo == "liquido") || (precio > premax && tipo == "liquido"));
+
+    {
+      premax = precio;
+      marcacaro = marca;
+      flag = 1;
+    }
+    if ((flagS == 0 && tipo == "solido") || (precio < premin && tipo == "solido"));
+    {
+      premin = precio;
+      marcabarato = marca;
+      flagS = 1;
+    }
+  }
+  document.write("El peso total de la compra es: " + pesototal + "<br>");
+  document.write("La marca más cara de los liquidos es :" + marcacaro + "<br>");
+  document.write("La marca del más barato de los solidos es: " + marcabarato + "<br>");
+} 
--- uno.js
+++ uno.js
@@ -9,5 +9,46 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let edad;
+	let sexo;
+	let peso;
+	let contM=0;
+	let edadprom=0;
+	let pesomax=0;
+	let nombrepeso;
+	let flag=0;
+	//let flaged=0;
+	
+	for(i=0;i<5;i++)
+	{
+		nombre=prompt("Ingrese su nombre");
+	 do{
+     edad=parseInt(prompt("Ingrese su edad "));
+	 }while(isNaN(edad)||edad<0);
+	 do
+	 {
+      sexo=prompt("Ingrese su sexo")
+	 }while(sexo!="f"&&sexo!="m");
+	 do{
+     peso=parseInt(prompt("Ingrese su peso"));
+	 }while(isNaN(peso));
+	 
+	 edadprom=edadprom+edad;
+	 if(sexo=="f")
+   {
+	   contM=contM+1;
+   }
+   if((flag==0&&sexo=="m")||(sexo="m"&&pesomax<peso))
+   {   nombrepeso=nombre;
+	   pesomax=peso;
+	   flag=1;
+   }
+   
+	}
+   
+   document.write("La cantidad de mujeres es: "+contM+"<br>");
+   document.write("El promedio de edad es: "+edadprom/5+"<br>");
+   document.write("El hombre mas pesado es: "+nombrepeso+"<br>");
+
 }
